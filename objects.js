ffbcb60e70c8445bfa9bd01addccb255683c587d
Damon Brown <dbrow437@gmail.com>
2018-02-08 22:36:12 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,5 @@
+var playlist = {artistName: "songTitles"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return updatePlaylist({}, playlist, 'Phil Ochs', 'Here\'s to the State of Mississippi')
+}
